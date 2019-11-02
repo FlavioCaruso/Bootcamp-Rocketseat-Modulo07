a9909de516a47a7addbc115ceb5bf903e6e1ee7f
Flavio <fbcaruso@hotmail.com>
2019-11-02 10:04:06 -0300

    Ajuste na Home com hooks

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,7 +20,8 @@ module.exports = {
   },
   plugins: [
     'react',
-    'prettier'
+    'prettier',
+    'react-hooks'
   ],
   rules: {
     'prettier/prettier': 'error',
@@ -31,6 +32,8 @@ module.exports = {
     'import/prefer-default-export': 'off',
     "react/state-in-constructor": [2, 'never'],
     'no-param-reassign': 'off',
-    'no-console': ["error", { allow: ["tron"] }]
+    'no-console': ["error", { allow: ["tron"] }],
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
 };
